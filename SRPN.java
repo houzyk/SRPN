6b7fc65df44debffbdafe5ab4769f6e847fbd107
houzairkoussa <houzairmk@icloud.com>
2022-02-04 15:00:13 +0400

    ADD utils

--- SRPN.java
+++ SRPN.java
@@ -10,12 +10,18 @@ public class SRPN {
   public SRPN () {
     // 1. initialises Views class for dealing with IO
     View view = new View();
+
     // 2. initialises random number generator from RandomNumClass
     RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
-    // 3. initialises array of arithmetic operations: -, +, ^, %, *, /
+
     Utils utils = new Utils();
+    // 3. initialises array of arithmetic operations: -, +, ^, %, *, /
     ArrayList<String> arithmeticOperations = utils.getArithmeticOperations();
-    // 4. welcomes user
+
+    // 4. initialises comment mode
+    boolean commentMode = utils.getCommentMode();
+
+    // 5. welcomes user
     view.welcomeUser();
   }
 
--- Utils.java
+++ Utils.java
@@ -3,15 +3,17 @@ import java.util.ArrayList;
 /**
  * Class for utils and helpers used by the SRPN calculator.
  * ? @arithmeticOperations is an array of all SRPN arithmetic operations
+ * ? @commentMode is stores whether a comment is present, true upon '#' command
  */
 
 public class Utils {
   ArrayList<String> arithmeticOperations = new ArrayList<>();
+  boolean commentMode = false; // initially, the calculator is NOT in comment mode
 
   // * constructor - executes when SRPN constructor is executed
   public Utils () {
     // fills arithmeticOperations array with all SRPN arithmetic operations
-    initArithmeticOperations();
+    initArithmeticOperationsArray();
   }
 
   // getter to obtain array of all arithmetic operations
@@ -19,8 +21,16 @@ public class Utils {
     return this.arithmeticOperations;
   }
 
+  // getter to obtain commentmode
+  public boolean getCommentMode () {
+    return this.commentMode;
+  }
+
+  // setter for comment mode. toggles comemnt mode on and off whenever a '#' is present
+  public void setCommentMode () { this.commentMode = !this.commentMode; }
+
   // adds each arithmetic operation to the arithmetic operations array
-  private void initArithmeticOperations () {
+  private void initArithmeticOperationsArray () {
     this.arithmeticOperations.add("+");
     this.arithmeticOperations.add("-");
     this.arithmeticOperations.add("*");
