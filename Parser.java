ce0aadaa4ee37690d23481ecc083404c5752ae7e
houzairkoussa <houzairmk@icloud.com>
2022-02-12 08:53:46 +0400

    ADD control flow

--- Parser.java
+++ Parser.java
@@ -1,6 +1,18 @@
 /**
  * Class for parsing user input into executable arithmetic operations in SRPN class
  */
-public class Parser {
 
+public abstract class Parser {
+  // takes user input divided by spaces and divides each input separated by a space
+  public static String[] parseCommandForSpaces(String command) {
+    return command.split("\\s+");
+  }
+
+  // A complex command is one that contains may operations within one string. It may look like 1+1+1. So, we take any complex command and separates them so that they can be handled by SRPN. So, 1+1+1 because 1 + 1 + 1 + 1 (notice the spaces).
+  public static String parseComplexSingleLineCommand(String complexCommand) {
+    return complexCommand.replaceAll(" +", " ")
+        .replaceAll("([\\p{P}\\p{S}a-z0-9])(?=[\\p{P}\\p{S}a-z])", "$1 ")
+        .replaceAll("([^- 0-9])(?=[0-9])", "$1 ")
+        .trim();
+  }
 }
--- SRPN.java
+++ SRPN.java
@@ -12,26 +12,59 @@ public class SRPN {
   Stack<Integer> numberStack = new Stack<>(); // numbers to be operated on by operations n operationQueue
   Queue<String> operationQueue = new LinkedList<>(); // operations to be performed on numberStack
 
+  Utils utils;
+  RandomNumGenerator randomNumGenerator;
+  ArrayList<String> arithmeticOperations;
+  boolean commentMode;
+
   // * constructor - executes the following steps when SRPN is initialised
   public SRPN () {
     // 1. initialises random number generator from RandomNumClass
-    RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
+    this.randomNumGenerator = new RandomNumGenerator();
 
     // 2. initialises utils
-    Utils utils = new Utils();
+    this.utils = new Utils();
 
     // 3. initialises array of arithmetic operations: -, +, ^, %, *, /
-    ArrayList<String> arithmeticOperations = utils.getArithmeticOperations();
+    this.arithmeticOperations = utils.getArithmeticOperations();
 
     // 4. initialises comment mode
-    boolean commentMode = utils.getCommentMode();
+    this.commentMode = this.utils.getCommentMode();
 
     // 5. welcomes user
     View.welcomeUser();
   }
 
   public void processCommand(String command) {
-
+    String[] parsedCommands = Parser.parseCommandForSpaces(command);
+    distributeCommandsForExecution (parsedCommands);
   }
 
+  private void distributeCommandsForExecution  (String[] parsedCommands) {
+    for (String command : parsedCommands) {
+      if (command.equals("#")) {
+        this.utils.setCommentMode();
+      } else {
+        distributeOperationsAndOperandsCommands(command);
+      }
+    }
+  }
+
+  private void distributeOperationsAndOperandsCommands (String command) {
+    if (!this.commentMode && !command.equals("")) {
+      if (command.matches("-?[0-9]+")) {
+
+      } else if (this.arithmeticOperations.contains(command)) {
+
+      } else if (command.equals("=")) {
+        View.printNumberStackTop(this.numberStack);
+      } else if (command.equals("d")) {
+        View.printNumberStack(this.numberStack);
+      } else if (command.equals("r")) {
+
+      } else {
+
+      }
+    }
+  }
 }
